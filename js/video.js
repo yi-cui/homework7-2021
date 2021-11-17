35c42da40601bda27219a8d8243debb24bc583ea
cuiyi <cuiyi08018232@gmail.com>
2021-11-16 19:55:33 -0500

    second commit

--- js/video.js
+++ js/video.js
@@ -9,6 +9,7 @@ window.addEventListener("load", function() {
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play()
+	document.querySelector('#volume').innerHTML = document.querySelector('#slider').value +'%'
 });
 
 document.querySelector('#pause').addEventListener("click", function() {
